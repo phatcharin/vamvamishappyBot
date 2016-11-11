e77ba3caf6e4ca1edd56b594b5d1d1253e089095
Phatcharin <snowlandvamvam@gmail.com>
2016-11-11 11:15:44 +0700

    hey

--- index.php
+++ index.php
@@ -16,12 +16,17 @@ if (!is_null($events['events'])) {
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 		
+				
+			if ($text == 'hi' || $text == 'Hi' || $text == 'HI') 
+			{
+				$text = "Hey!!"
+			}
 
-				// Build message to reply back
-				$messages = [
-					'type' => 'text',
-					'text' => $text
-				];
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 			
 
 			// Make a POST Request to Messaging API to reply to sender
